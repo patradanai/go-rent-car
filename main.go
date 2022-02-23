1e163ec2f34b3756abdd03c3eabda10ebdbfae11
digio <=>
2022-02-24 02:00:54 +0700

    initial router

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"car-booking/configs"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
 func main() {
+	router := gin.Default()
 	if _, err := configs.ConnectToDB(); err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
 		panic(err)
 	}
 }
